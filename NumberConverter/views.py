bbb4ee9c47740caf79ce193536b444e4bb9a6772
Sakii Mia <Sakilmia@gmail.com>
2020-11-25 08:24:18 -0600

    change the result header

--- NumberConverter/views.py
+++ NumberConverter/views.py
@@ -14,27 +14,27 @@ def result(request):
         decimal = int(djtext, 2)
         octal = oct(decimal)
         hexa = hex(decimal)
-        params = {'type1' : 'Decimal','value1':decimal,'type2':'Octal (octal code:0o)','value2':octal,'type3':'Hexa Decimal(hexa decimal code:0x)','value3':hexa}
+        params = {'type1' : 'Decimal','value1':decimal,'type2':'Octal (octal code:0o)','value2':octal,'type3':'Hexa Decimal (hexa decimal code:0x)','value3':hexa}
         return render(request, 'result.html', params)
     elif selecttype == 'octal' and djtext != '':
         decimal = str(int(djtext, 8))
         binary = bin(int(decimal))
         hexa = hex(int(decimal))
-        params = {'type1': 'Decimal', 'value1': decimal, 'type2': 'Binary', 'value2': binary, 'type3': 'Hexa Decimal',
+        params = {'type1': 'Decimal', 'value1': decimal, 'type2': 'Binary (binary code:0b)', 'value2': binary, 'type3': 'Hexa Decimal (hexa decimal code:0x)',
                   'value3': hexa}
         return render(request, 'result.html', params)
     elif selecttype == 'decimal' and djtext != '':
         octal = oct(int(djtext))
         binary = bin(int(djtext))
         hexa = hex(int(djtext))
-        params = {'type1': 'Octal', 'value1': octal, 'type2': 'Binary', 'value2': binary, 'type3': 'Hexa Decimal',
+        params = {'type1': 'Octal (octal code:0o)', 'value1': octal, 'type2': 'Binary (binary code:0b)', 'value2': binary, 'type3': 'Hexa Decimal (hexa decimal code:0x)',
                   'value3': hexa}
         return render(request, 'result.html', params)
     elif selecttype == 'hexa' and djtext != '':
         decimal = str(int(djtext, 16))
         binary = bin(int(decimal))
         octal = oct(int(decimal))
-        params = {'type1': 'Decimal', 'value1': decimal, 'type2': 'Binary', 'value2': binary, 'type3': 'Octal',
+        params = {'type1': 'Decimal', 'value1': decimal, 'type2': 'Binary (binary code:0b)', 'value2': binary, 'type3': 'Octal (octal code:0o)',
                   'value3': octal}
         return render(request, 'result.html', params)
     else:
